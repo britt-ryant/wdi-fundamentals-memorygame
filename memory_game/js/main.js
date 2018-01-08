860c6a93335b1a8400505f96538cbd2d18a2e569
Ryan Britt <ryanbritt@Ryans-MBP.home>
2018-01-08 09:42:17 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and Running!")
+
+var cardOne = "King";
+var cardTwo = "King";
+var cardThree = "Queen";
+var cardFour = "Queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
+
