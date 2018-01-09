4c7042f249f962a5463fb4136aa8bea4e8987396
Ryan Britt <ryanbritt@Ryans-MBP.home>
2018-01-09 11:47:20 -0500

    Added new js using functions to determine if card is a match and log to console.  Added flipCardand checkForMatch function

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -15,23 +15,40 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
+var checkForMatch = function () {
+	
 if (cardsInPlay.length === 2) {
-	//console.log("yup");
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.")
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.")
+		}
 	}
 }
+
+
+var flipCard = function (cardId) {
+	/*
+	var cardOne = cards[0];
+	cardsInPlay.push(cardOne);
+	console.log("User flipped " + cardOne);
+
+	var cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cardTwo);
+	*/
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+	/*
+	if (cardsInPlay.length === 2) {
+		//console.log("yup");
+	} 
+	*/
+}
+
+
+flipCard(1);
+flipCard(2);
