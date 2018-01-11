460e01bee35f7566c938c61159c78419d8445bc0
Ryan Britt <ryanbritt@Ryans-MBP.home>
2018-01-11 10:19:09 -0500

    fixed code error that wasnt apperent until final code was added, added final JS, added reset button, added score tracker, added comments to code.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,6 @@
 
-/*
-console.log("Up and Running!")
 
-var cardOne = "King";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "Queen";
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
-*/
+//Defining the cards in an array
 
 var cards = [
 {
@@ -33,39 +24,71 @@ var cards = [
 
 var cardsInPlay = [];
 
+
+var scoreIncrementer = 1;
+
+//function to check if the identities of the cards match
+
 var checkForMatch = function () {
 	
-if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
+			document.getElementById('scoreNumber').innerHTML = scoreIncrementer++;
 		} else {
 			alert("Sorry, try again.")
 		}
-	}
 }
 
 
-var flipCard = function (cardId) {
-	/*
-	var cardOne = cards[0];
-	cardsInPlay.push(cardOne);
-	console.log("User flipped " + cardOne);
+//function that gets the attrubute of the card that was clicked, checks for a match to previous card (if applicable), and displays image of the card that is clicked. 
 
-	var cardTwo = cards[2];
-	cardsInPlay.push(cardTwo);
-	console.log("User flipped " + cardTwo);
-	*/
-	console.log("User flipped " + cards[cardId].rank);
+var flipCard = function () {
 
-	console.log(cards[cardId].cardImage);
-
-	console.log(cards[cardId].suit);
+	var cardId = this.getAttribute('data-id');
 
 	cardsInPlay.push(cards[cardId].rank);
 
+	this.setAttribute('src', cards[cardId].cardImage);
+
+	if (cardsInPlay.length === 2) {
+
 	checkForMatch();
+
+	cardsInPlay = [];
+
+	}
+}
+
+//function to create the images of the back of the card.  Function also includes a listeneing event for the user to click the card.  Imbedded within the function is a for loop that cycles through all the card elements in the array.
+
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);	
+	};
+}
+
+//function to reset the image on the card AND if the user hits reset while only one card is in play, resets the cardsInPlay array.
+
+var flipCardBack = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var showBack = document.getElementsByTagName('img')[i];
+		showBack.setAttribute('src', "images/back.png");
+	}
+	cardsInPlay.pop();
+}
+
+var resetBoard = function () {
+		var resetButton = document.getElementById('reset');
+		resetButton.addEventListener('click', flipCardBack);
 }
 
 
-flipCard(0);
-flipCard(2);
+
+
+resetBoard();
+createBoard();
+
